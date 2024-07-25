aabab9efc164dc75e876d26e1789c96fd4d0fe0d
Leonardo Rene <leonardorene.dev@gmail.com>
2024-07-24 23:30:42 -0300

    arrays explicado

---
+++ arrays_foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exemplificando um array...");
+
+Console.WriteLine("Quantos nomes vc quer adicionar no sistema? Informe aqui: ...");
+int qtdeNomes = int.Parse(Console.ReadLine()!);
+if(qtdeNomes > 0){
+string[] nomes = new string[qtdeNomes];
+
+    for(int posicaoNome = 0; posicaoNome < qtdeNomes; posicaoNome++){
+        Console.WriteLine("Informe o nome: ...");
+        nomes[posicaoNome] = Console.ReadLine()!;
+    }
+    
+    List<string> list = nomes.ToList();
+    list.Add("DeafTech");
+    foreach(string nome in list) Console.WriteLine("Voce digitou o nome {0}",nome);
+
+
+}
+
+
+
